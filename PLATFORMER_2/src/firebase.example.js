5542871efa99272b3d95a6b418968a67c7dcb290
SamuFer <fernandezSamuel0107@gmail.com>
2026-03-16 21:13:58 +0100

    Corrigiendo errores del gitignore

---
+++ PLATFORMER_2/src/firebase.example.js
@@ -0,0 +1,18 @@
+// // firebase.js
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// // ✅ Aquesta és la teva configuració personal
+// const firebaseConfig = {
+//   apiKey: "tu apiKey aquí de Firebase",
+//   authDomain: "tu authDomain aquí de Firebase",
+//   projectId: "tu projectId aquí de Firebase",
+//   storageBucket: "tu storageBucket aquí de Firebase",
+//   messagingSenderId: "tu messagingSenderId aquí de Firebase",
+//   appId: "tu appId aquí de Firebase",
+//   measurementId: "tu measurementId aquí de Firebase"
+// };
+
+// // Inicialitza Firebase i exporta Firestore
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
